af14379456f9a9072ff1f19f8d3d3e986fc02d69
yaotutu <957810668@qq.com>
2023-01-08 09:23:47 +0000

    添加 antd,sass支持

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Button } from 'antd-mobile'
 
 function App() {
   return (
     <div className="App">
       <h1>haha</h1>  
+      <Button color='primary' fill='solid'>
+            Solid
+          </Button>
     </div>
   );
 }
