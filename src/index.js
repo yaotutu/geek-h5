a00872790a68639c95b505c8e2410ff7fd6762fe
yaotutu <957810668@qq.com>
2023-01-09 21:54:19 +0800

    优化代码结构

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./assets/styles/index.scss"
+import "@/assets/styles/index.scss"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
