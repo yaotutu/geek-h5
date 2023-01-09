ca85317cf232a58c44d773a1e8e28f3a30c13a8e
yaotutu <957810668@qq.com>
2023-01-09 21:53:59 +0800

    配置别名@

--- config-overrides.js
+++ config-overrides.js
@@ -1,2 +1,11 @@
-const { override } = require('customize-cra');
-module.exports = {};
+const { override, addWebpackAlias } = require("customize-cra");
+const path = require("path");
+
+// 配置别名
+const alias =   addWebpackAlias({
+    "@": path.join(__dirname, "src"),
+    "@scss": path.join(__dirname, "src/assets/styles"),
+  })
+module.exports = override(
+    alias
+);
