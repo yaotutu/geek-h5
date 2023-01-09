d1548bf0ca44cd52a1d7f9a064794e8c4f25a412
yaotutu <957810668@qq.com>
2023-01-09 22:52:33 +0800

    配置postcss，遗留问题，postcss未生效

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,19 @@
-const { override, addWebpackAlias } = require("customize-cra");
+const { override, addWebpackAlias, addPostcssPlugins } = require("customize-cra");
 const path = require("path");
+const px2viewport = require("postcss-px-to-viewport")
 
 // 配置别名
 const alias =   addWebpackAlias({
     "@": path.join(__dirname, "src"),
     "@scss": path.join(__dirname, "src/assets/styles"),
   })
+//配置postCss
+const postcssPlugins = addPostcssPlugins([
+    px2viewport({
+        viewportWidth: 375
+    })
+])
 module.exports = override(
-    alias
+    alias,
+    postcssPlugins
 );
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Button } from 'antd-mobile'
+import "@/App.css"
 
 function App() {
   return (
     <div className="App">
       <h1>haha</h1>  
+      <div className="demo">
+dfa
+      </div>
       <Button color='primary' fill='solid'>
             Solid
           </Button>
